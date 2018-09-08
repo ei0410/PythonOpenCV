f0cef70ae9f1c13bb8860bb54dedda4adaa84533
ei <chronolavos0124@gmail.com>
2018-09-08 12:52:32 +0900

    Add flip.py

---
+++ cameras/flip.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+def main():
+    cap = cv2.VideoCapture(0)
+    
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        frame = cv2.flip(frame, 0)
+
+        cv2.imshow("raw", frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
--- cameras/rotate.py
+++ cameras/rotate.py
@@ -3,13 +3,14 @@ import numpy as np
 
 def main():
     cap = cv2.VideoCapture(0)
+
+    theta = 0
     
     while(cap.isOpened()):
         ret, frame = cap.read()
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        theta = 180
         scale = 1.0
 
         oy, ox = int(gray.shape[0]/2), int(gray.shape[1]/2)
@@ -18,6 +19,8 @@ def main():
         dst = cv2.warpAffine(gray, R, gray.shape, flags=cv2.INTER_CUBIC)
 
         cv2.imshow("raw", dst)
+
+        theta = theta + 1
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
