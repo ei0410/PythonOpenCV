e20aa2fbb9922a4058f38a84aad6c3b5ac669138
ei <chronolavos0124@gmail.com>
2018-10-24 20:07:21 +0900

    add save path and directory

--- cameras/man.py
+++ cameras/man.py
@@ -5,6 +5,8 @@ def main():
     cap = cv2.VideoCapture(0)
 
     cascade = cv2.CascadeClassifier("haarcascade_fullbody.xml")
+
+    save_path = "./outputs/"
     
     while(cap.isOpened()):
         ret, frame = cap.read()
@@ -13,8 +15,11 @@ def main():
 
         man = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(30, 30))
 
-        for (x, y, w, h) in face:
+        for (x, y, w, h) in man:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 200), 3)
+
+        if len(man) > 0:
+            cv2.imwrite(save_path + "output.jpg", frame)
 
         cv2.imshow("image", frame)
         
