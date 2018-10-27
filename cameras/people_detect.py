125ab5346461da69920d8d4a5a0c49245a491c77
ei <chronolavos0124@gmail.com>
2018-10-27 17:04:22 +0900

    bug fixed

--- cameras/people_detect.py
+++ cameras/people_detect.py
@@ -65,8 +65,8 @@ def use_cascade(frame, face_cascade):
 
         dst = frame[my:my+mh, mx:mx+mw]
 
-        if (mx < fx) and (my - int(mh/4)< fy) and (mx + mw > fx + fw) and (my + mh > fy + fh):
-            dst = frame[my-int(mh/4):my+mh, mx:mx+mw]
+        if (mx < fx) and (my - int(mh/5)< fy) and (mx + mw > fx + fw) and (my + mh > fy + fh):
+            dst = frame[my-int(mh/5):my+mh, mx:mx+mw]
             #cv2.imwrite(save_path + now + ".jpg", frame)
             cv2.imwrite(save_path + now + ".jpg", dst)
 
